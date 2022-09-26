6ab5fe463ffe92ba2586c3083a337cf1a595c99a
Chikkl <Pavel.schershnev@yandex.ru>
2022-09-26 13:55:38 -0400

    First Commit xD

---
+++ main.py
@@ -0,0 +1,12 @@
+from termcolor import colored
+
+
+def main():
+    color_dict ={0: "red", 1: "yellow", 2: "green", 3: "blue", 4: "magenta", 5: "cyan"}
+    
+    for index, value in enumerate("Hello world!"):
+        print(colored(value, color_dict[index%6]), end="")
+
+    
+if __name__ == "__main__":
+    main()
