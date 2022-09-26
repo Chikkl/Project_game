0a25dbd222002b6d8a5a2f2da605b9c7df525df4
Chikkl <Pavel.schershnev@yandex.ru>
2022-09-26 14:39:22 -0400

    test commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from termcolor import colored
 
 
 def main():
-    color_dict ={0: "red", 1: "yellow", 2: "green", 3: "blue", 4: "magenta", 5: "cyan"}
+    color_dict = {0: "red", 1: "yellow", 2: "green", 3: "blue", 4: "magenta", 5: "cyan"}
     
     for index, value in enumerate("Hello world!"):
         print(colored(value, color_dict[index%6]), end="")
